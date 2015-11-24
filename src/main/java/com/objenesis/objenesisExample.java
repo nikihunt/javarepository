9f4fac843ffbe8e7e3f1587cf6fd3dd931895a71
nikihunt <759640248@qq.com>
2015-11-24 18:34:10 +0800

    first commit

---
+++ src/main/java/com/objenesis/objenesisExample.java
@@ -0,0 +1,29 @@
+package com.objenesis;
+
+
+import org.objenesis.Objenesis;
+import org.objenesis.ObjenesisStd;
+import org.objenesis.instantiator.ObjectInstantiator;
+
+import static com.util.Print.println;
+/**
+ * Created by dell on 2015/11/24.
+ */
+public class objenesisExample {
+    class Test{
+        private String user;
+        private String passwd;
+        public Test() {
+            println("non-arg constructor is called!");
+        }
+        public void say(){
+            println("hello");
+        }
+    }
+    public static void main(String[] args){
+        Objenesis objenesis = new ObjenesisStd();
+        ObjectInstantiator instantiator = objenesis.getInstantiatorOf(Test.class);
+        Test t = (Test)instantiator.newInstance();
+        t.say();
+    }
+}
---
+++ src/main/java/com/util/Print.java
@@ -0,0 +1,19 @@
+package com.util;
+
+/**
+ * Created by dell on 2015/11/24.
+ */
+public class Print {
+    public static void print(){
+        System.out.print("");
+    }
+    public static void print(String str){
+        System.out.print(str);
+    }
+    public static void println(){
+        System.out.println("");
+    }
+    public static void println(String str){
+        System.out.println(str);
+    }
+}
