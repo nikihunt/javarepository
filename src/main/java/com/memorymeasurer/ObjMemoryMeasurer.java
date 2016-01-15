7ab5c889b023f8cd9bafd78e197f40ccd289c8bc
nikihunt <759640248@qq.com>
2016-01-15 21:11:53 +0800

    run memorymeasurer success

---
+++ src/main/java/com/memorymeasurer/ObjMemoryMeasurer.java
@@ -0,0 +1,25 @@
+package com.memorymeasurer;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import objectexplorer.MemoryMeasurer;
+
+import java.util.List;
+import java.util.Map;
+
+import static com.util.Print.*;
+
+/**
+ * Created by zl on 16/1/15.
+ */
+
+
+public class ObjMemoryMeasurer {
+    public static void main(String[] args){
+        Map<String,String> map = Maps.newHashMap();
+        List<String> lst = Lists.newArrayList();
+        Long a = 0L;
+        long memory = MemoryMeasurer.measureBytes(a);
+        print(Long.toString(memory));
+    }
+}
---
+++ src/main/java/test/ESTest.java
@@ -0,0 +1,15 @@
+package test;
+
+import com.Log.LogFactory;
+import org.apache.log4j.Logger;
+
+/**
+ * Created by zl on 16/1/15.
+ */
+
+
+public class ESTest {
+    private static final Logger logger = LogFactory.getInstance().getLogger(ESTest.class);
+
+    public ESTest(){}
+}
